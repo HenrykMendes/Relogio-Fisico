058a2a49f7cbddf325884c068cdc6de0f2192589
J. Henrique Mendes <h06.987654321@gmail.com>
2026-03-23 23:25:38 -0300

    feat: Definição das váriaveis e criação do método construtor

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,24 @@
+import java.io.*;
+import java.net.Socket;
+import java.sql.Timestamp;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+
 public class Client {
+    private String nomeDoServidor; // Nome do servidor
+    private int PortaDoServidor; // Valor da porta
+    private static int count; // Número de "clientes"(conexões)
+    private Timer temporizador;
+    private PrintWriter logDescricao;
+    private long tempo1;
+    private long tempo2;
+
+    //  Método Construtor
+    public Client(String nomeDoServidor, int portaDoServidor, Timer temporizador) {
+        this.nomeDoServidor = nomeDoServidor;
+        this.PortaDoServidor = portaDoServidor;
+        this.temporizador = new Timer();
+        Client.count = 0;
+    }
 }
